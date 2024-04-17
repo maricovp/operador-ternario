7e94098bcf4f86a2e54b6a4e5247bd59e7c4acc5
maricovp <marcos2015vinicuis@gmail.com>
2024-04-17 09:54:54 -0400

    adicionado atividade7.js

---
+++ atividade7.js
@@ -0,0 +1,10 @@
+/*Escreva um código JavaScript utilizando o operador ternário para 
+determinar se um número é maior que 100. A variável numero contém o valor 
+a ser verificado. Atribua o resultado à variável resultado. */
+let numero = 120;
+
+let resultado = numero > 100 ? "é maior que 100" : "não é maior que 100";
+
+console.log(`O número ${numero} ${resultado}.`);
+
+
