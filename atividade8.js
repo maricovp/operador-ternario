f4f74518a040a98c1d3fdd1aa6edb6655ce250e9
maricovp <marcos2015vinicuis@gmail.com>
2024-04-17 09:54:58 -0400

    adicionado atividade8.js

---
+++ atividade8.js
@@ -0,0 +1,9 @@
+/* Escreva um código JavaScript utilizando o operador ternário para 
+determinar se um número é divisível por 3. A variável numero contém o valor a 
+ser verificado. Atribua o resultado à variável resultado*/
+
+let numero = 12; 
+let resultado = numero % 3 === 0 ? "é divisível por 3" : "não é divisível por 3";
+
+console.log(`O número ${numero} ${resultado}.`);
+
